92562168c7e14dac4269a18285f057a3a39f4147
necromant1k <necromant1k96@gmail.com>
2018-01-11 02:05:08 +0300

    Add cohort ui

--- src/routes/Cohorts/containers/CohortList.js
+++ src/routes/Cohorts/containers/CohortList.js
@@ -1,28 +1,51 @@
 import React, { cloneElement } from "react";
 import PropTypes from "prop-types";
+import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { withHandlers } from "recompose";
 import { withFirebase } from "react-redux-firebase";
+import { required, validateEmail } from "utils/form";
 import { spinnerWhileLoading } from "utils/components";
 import { UserIsAuthenticated } from "utils/router";
 import classes from "./CohortList.scss";
-import { Grid, Row, Col, Table, ProgressBar } from "react-bootstrap";
+import {
+  Grid,
+  Row,
+  Col,
+  Table,
+  ProgressBar,
+  Modal,
+  Button,
+  FormGroup
+} from "react-bootstrap";
 import { firebaseConnect, populate } from "react-redux-firebase";
 import { map, get } from "lodash";
 import { COHORT_LIST } from "constants";
+import { FormInput } from "../../../components/FormInput";
 
 export class CohortList extends React.Component {
   static contextTypes = {
     router: PropTypes.object.isRequired
   };
+
+  state = {
+    isNew: false
+  };
+
+  handleSubmit(form) {
+    console.log(form);
+  }
+
   render() {
     if (this.props.children) {
       return cloneElement(this.props.children, this.props);
     }
 
     const { cohorts } = this.props;
+    const { isNew } = this.state;
+
     return (
       <Grid className={classes.container}>
         <Row>
@@ -34,24 +57,79 @@ export class CohortList extends React.Component {
             mdOffset={4}
             className={classes.cardWrapper}
           >
-            {cohorts &&
-              cohorts.length && (
-                <div>
-                  <h1 className="text-center">
-                    <i className="fa fa-close" />
-                    Select Cohort
-                  </h1>
-                  <div>
-                    {map(cohorts, (cohort, key) => (
-                      <Link className={classes.button}>{cohort.name}</Link>
-                    ))}
-                  </div>
+            {!isNew && (
+              <div>
+                {cohorts &&
+                  cohorts.length && (
+                    <div>
+                      <h1 className="text-center">
+                        <i className="fa fa-close" />
+                        Select Cohort
+                      </h1>
+                      <div>
+                        {map(cohorts, (cohort, key) => (
+                          <Link className={classes.button}>{cohort.name}</Link>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                <div
+                  className={classes.addCohort}
+                  onClick={() => this.setState({ isNew: true })}
+                >
+                  <i className="fa fa-plus" />
+                  Add Cohort
                 </div>
-              )}
-            <div className={classes.addCohort}>
-              <i className="fa fa-plus" />
-              Add Cohort
-            </div>
+              </div>
+            )}
+            {isNew && (
+              <div className={classes.cardWrapper}>
+                <div className={classes.card}>
+                  <span
+                    onClick={() => this.setState({ isNew: false })}
+                    className={classes.closeNew}
+                  >
+                    <i className="fa fa-close" />
+                  </span>
+                  <form
+                    className={classes.container}
+                    onSubmit={this.handleSubmit}
+                  >
+                    <FormGroup>
+                      <Field
+                        className={classes.formField}
+                        name="institution"
+                        type="text"
+                        placeholder="Institution name"
+                        component={FormInput}
+                        validate={value => (value ? undefined : "Required")}
+                      />
+                      <Field
+                        className={classes.formField}
+                        name="instructor"
+                        type="text"
+                        placeholder="Instructor's email"
+                        component={FormInput}
+                        validate={value => (value ? undefined : "Required")}
+                      />
+                      <Field
+                        className={classes.formField}
+                        name="section"
+                        type="text"
+                        placeholder="Class section"
+                        component={FormInput}
+                        validate={value => (value ? undefined : "Required")}
+                      />
+                    </FormGroup>
+                    <div className={classes.actionContainer}>
+                      <Button bsStyle="primary" type="submit" disabled>
+                        Add Cohort
+                      </Button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            )}
           </Col>
         </Row>
       </Grid>
@@ -78,5 +156,8 @@ export default compose(
   connect(state => ({
     cohorts: state.firebase.data.cohorts
   })),
-  spinnerWhileLoading(["cohorts"])
+  spinnerWhileLoading(["cohorts"]),
+  reduxForm({
+    form: "COHORT_LIST"
+  })
 )(CohortList);
